7bec9bd9e1d0d57e6c98e59480d92afef3b002fa
Ganesh <BIONISCHE@192.168.1.105>
2018-11-24 12:43:38 +0530

    //Ganesh

---
+++ src/main/java/com/bionische/swastiktruckage/SwastikTruckageApplication.java
@@ -0,0 +1,12 @@
+package com.bionische.swastiktruckage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwastikTruckageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwastikTruckageApplication.class, args);
+	}
+}
