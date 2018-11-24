317613cdc227cacaeef219988c391856309632fb
BIONISCHE <BIONISCHE@192.168.1.105>
2018-11-24 14:38:33 +0530

    //Ganesh

--- src/main/java/com/bionische/swastiktruckage/mastermodel/CompanyDetails.java
+++ src/main/java/com/bionische/swastiktruckage/mastermodel/CompanyDetails.java
@@ -1,18 +1,19 @@
 package com.bionische.swastiktruckage.mastermodel;
 
-import java.sql.Date;
+
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
