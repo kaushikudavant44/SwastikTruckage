0684b21ca3b9721423ff2259454f36c54952705f
Admin <Admin@Bionische2>
2018-12-04 16:16:55 +0530

    //by amruta

--- src/main/java/com/bionische/swastiktruckage/repository/LrBillingRepository.java
+++ src/main/java/com/bionische/swastiktruckage/repository/LrBillingRepository.java
@@ -11,9 +11,9 @@ import com.bionische.swastiktruckage.model.GetStaffDetails;
 
 public interface LrBillingRepository extends JpaRepository<LrBilling,Integer>{
 	
-	@Query(value="SELECT h.lr_header_id,h.consignor,h.lr_id,h.inv_header_id,h.from_id,h.to_id,h.lr_date,h.truck_no,h.weight,h.freight,h.gst,h.hamali,"
-			+ "h.local_tempo,h.total,h.payment_by,h.status,SUM(c.no_of_containts) AS quantity FROM t_lr_header h,t_lr_containt_details c,t_lr_invoice_detail i"
-			+ " WHERE h.lr_header_id=c.lr_header_id AND h.consignor=:clientId AND h.status=0 GROUP BY h.lr_header_id " ,nativeQuery=true)
+	@Query(value="SELECT h.lr_header_id,h.consignor,h.lr_id,h.inv_header_id,h.from_id,h.to_id,h.lr_date,h.truck_no,h.weight,h.freight,h.gst,h.hamali,h.local_tempo,h.total,h.payment_by,\r\n" + 
+			"h.status,SUM(c.no_of_containts) AS quantity FROM t_lr_header h,t_lr_containt_details c WHERE h.lr_header_id=c.lr_header_id AND h.consignor=:clientId AND \r\n" + 
+			"h.status=0 GROUP BY h.lr_header_id " ,nativeQuery=true)
 	List<LrBilling> getBillDetailByClientId(@Param("clientId") int clientId);
 	
 
