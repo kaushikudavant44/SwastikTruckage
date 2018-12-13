f74f2aee43d42f24e43811f572b6a1a0464225b4
Jarves <Jarves@DESKTOP-TLMVIJ2>
2018-12-13 19:08:17 +0530

    //kaushik Memo controller

--- src/main/java/com/bionische/swastiktruckage/controller/MemoController.java
+++ src/main/java/com/bionische/swastiktruckage/controller/MemoController.java
@@ -8,22 +8,30 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bionische.swastiktruckage.mastermodel.GetAllLrDetails;
+import com.bionische.swastiktruckage.mastermodel.Goods;
 import com.bionische.swastiktruckage.mastermodel.Info;
+import com.bionische.swastiktruckage.mastermodel.LRDetails;
+import com.bionische.swastiktruckage.mastermodel.LrContaintDetails;
 import com.bionische.swastiktruckage.mastermodel.MemoDetails;
 import com.bionische.swastiktruckage.mastermodel.MemoHeader;
 import com.bionische.swastiktruckage.mastermodel.OfficeDetails;
 import com.bionische.swastiktruckage.mastermodel.OfficeStaff;
 import com.bionische.swastiktruckage.mastermodel.TransactionLrHeader;
+import com.bionische.swastiktruckage.mastermodel.TransactionLrInvoiceDetail;
 import com.bionische.swastiktruckage.mastermodel.VehicleDetails;
 import com.bionische.swastiktruckage.mastermodel.VehicleOwners;
 import com.bionische.swastiktruckage.mastermodel.VehiclesDrivers;
+import com.bionische.swastiktruckage.model.GetAllMemo;
+import com.bionische.swastiktruckage.model.ShowAllMemo;
 import com.bionische.swastiktruckage.repository.GetAllLrDetailsRepository;
+import com.bionische.swastiktruckage.repository.GetAllMemoRepository;
 import com.bionische.swastiktruckage.repository.MemoDetailsRepository;
 import com.bionische.swastiktruckage.repository.MemoHeaderRepository;
 import com.bionische.swastiktruckage.repository.OfficeDetailsRepository;
@@ -62,6 +70,9 @@ public class MemoController {
 	
 	@Autowired
 	TransactionLrHeaderRepository transactionLrHeaderRepository;
+	
+	@Autowired
+	GetAllMemoRepository getAllMemoRepository;
 	
 	List<VehicleDetails> vehicalDetailsList=new ArrayList<>();
 	
@@ -149,7 +160,7 @@ public class MemoController {
 			}
 			}
 				
-				
+			
 			memoHeader.setMemoNo(memoNo);
 			memoHeader.setDriverId(Integer.parseInt(request.getParameter("driverId")));
 			memoHeader.setOfficeId(Integer.parseInt(request.getParameter("officeId")));
@@ -166,7 +177,7 @@ public class MemoController {
 			List<MemoDetails> memoDetailsList=new ArrayList<>();
 		
 			
-			String[] memoHeaderId = getAllMemoHeader.split(",");;
+			String[] memoHeaderId = getAllMemoHeader.split(",");
 			
 			for(int i=0;i<memoHeaderId.length;i++) {
 			
@@ -198,6 +209,171 @@ public class MemoController {
 				
 				info.setMessage("Memo created Successfully");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return info;
+
+	}
+	
+	@RequestMapping(value = "/showMemo", method = RequestMethod.GET)
+
+	public ModelAndView showMemo(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("memo/showMemoDetails");
+
+		List<GetAllMemo> getAllMemoList=new ArrayList<>();
+		
+		try {
+			getAllMemoList=getAllMemoRepository.getListOfMemo();
+			
+			
+			
+			model.addObject("getAllMemoList", getAllMemoList);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/editMemoDetails/{memoHeaderId}", method = RequestMethod.GET)
+
+	public ModelAndView showLrPreview(HttpServletRequest request, @PathVariable int memoHeaderId) {
+		ModelAndView model = new ModelAndView("memo/editMemo");
+
+		
+		try {
+			List<GetAllLrDetails> lrDetailsList=new ArrayList<>();
+			HttpSession session = request.getSession();
+			OfficeStaff officeStaff=(OfficeStaff) session.getAttribute("staffDetails");
+			int staffId = officeStaff.getStaffId();
+			OfficeStaff staffDetails = new OfficeStaff();
+			GetAllMemo getMemoDeatails=new GetAllMemo();
+			vehicalDetailsList.clear();
+			
+			List<VehiclesDrivers> vehicleDriverList=new ArrayList<>();
+			
+			vehicalDetailsList=vehicleDetailsRepository.findByIsUsed(true);
+			List<OfficeDetails> officeList = officeDetailsRepository.findByIsUsed(true);
+			VehicleOwners vehicleOwners = new VehicleOwners();
+			staffDetails = officeStaffRepository.findByStaffId(staffId);
+			vehicleDriverList=vehiclesDriversRepository.findByIsUsed(true);
+			
+			//vehicleOwner=vehicleOwnersRepository.findByIsUsed(true);
+			getMemoDeatails=getAllMemoRepository.findByMemoHeaderId(memoHeaderId);
+		//	staffDetails = officeStaffRepository.findByStaffId(staffId);
+			lrDetailsList=getAllLrDetailsRepository.getMemoLrDetailsByMemoHeaderId(memoHeaderId);
+			
+			
+			
+			for(int i=0;i<vehicalDetailsList.size();i++) {
+				if(vehicalDetailsList.get(i).getVehId()==getMemoDeatails.getVehId()) {
+					
+					int ownerId=vehicalDetailsList.get(i).getOwnerId();
+					vehicleOwners=vehicleOwnersRepository.findByOwnerId(ownerId);
+				}
+				
+			}
+			
+			
+			
+			
+			
+			
+			model.addObject("vehicleOwners", vehicleOwners);
+			model.addObject("vehicalDetailsList", vehicalDetailsList);
+			model.addObject("officeList", officeList);
+			model.addObject("vehicleDriverList", vehicleDriverList);
+			model.addObject("lrDetailsList", lrDetailsList);
+			model.addObject("getMemoDeatails", getMemoDeatails);
+			model.addObject("staffDetails", staffDetails);
+		
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return model;
+
+	}
+	
+	@RequestMapping(value = "/deleteMemo/{memoHeaderId}", method = RequestMethod.GET)
+
+	public String deleteLr(HttpServletRequest request,@PathVariable int memoHeaderId) {
+		ModelAndView model = new ModelAndView("transaction/showLrDetails");
+
+			MemoHeader memoHeader=new MemoHeader();
+		
+			
+		try {
+			int isUsed=memoHeaderRepository.updateMemoByMemoHeaderId(memoHeaderId);
+			
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return "redirect:/showLrDetails";
+
+	}
+	
+	
+	@RequestMapping(value = "/editMemoDetails", method = RequestMethod.GET)
+	public @ResponseBody Info editMemoDetails(HttpServletRequest request) {
+		
+		Info info=new Info();
+
+		MemoHeader memoHeader=new MemoHeader();
+		
+		
+ 		vehicalDetailsList.clear();
+		try {
+			
+			vehicalDetailsList=vehicleDetailsRepository.findByIsUsed(true);
+			
+			String vehicalNo=request.getParameter("ownerId");
+			for(int i=0;i<vehicalDetailsList.size();i++) {
+			if(vehicalNo.equals(vehicalDetailsList.get(i).getVehNo())) {
+				
+				memoHeader.setVehId(vehicalDetailsList.get(i).getVehId());
+			}
+			}
+				
+			memoHeader.setMemoHeaderId(Integer.parseInt(request.getParameter("memoHeaderId")));
+			memoHeader.setMemoNo(Integer.parseInt(request.getParameter("memoNo")));
+			
+			memoHeader.setDriverId(Integer.parseInt(request.getParameter("driverId")));
+			memoHeader.setOfficeId(Integer.parseInt(request.getParameter("officeId")));
+			memoHeader.setStaffId(Integer.parseInt(request.getParameter("staffId")));
+			
+			memoHeader.setUsed(true);
+			MemoHeader memoHeaderResult=memoHeaderRepository.save(memoHeader);
+			System.out.println("memoHeaderResult"+memoHeaderResult.toString());
+			
+			String getLrHeaderId=request.getParameter("unSelectedRowList");
+			String getAllLrHeader=getLrHeaderId.substring(1, getLrHeaderId.length()-1);
+			
+			System.out.println(getAllLrHeader.toString());
+			
+			String[] memoLrId = getAllLrHeader.split(",");
+			int deleteMemoDetails=0;
+			
+			for(int i=0;i<memoLrId.length;i++) {
+			
+				int lrHeaderId=Integer.parseInt(memoLrId[i]);
+				deleteMemoDetails=memoDetailsRepository.deleteMemoDetailsByLrHeaderId(lrHeaderId);
+					
+			}
+			if(deleteMemoDetails==1) {
+				
+				info.setMessage("Record Update Successfully");
+			}else {
+				info.setMessage("something went wrong");
+			}
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/bionische/swastiktruckage/repository/GetAllLrDetailsRepository.java
+++ src/main/java/com/bionische/swastiktruckage/repository/GetAllLrDetailsRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bionische.swastiktruckage.mastermodel.GetAllLrDetails;
 
@@ -16,7 +17,7 @@ public interface GetAllLrDetailsRepository extends JpaRepository<GetAllLrDetails
 			"FROM t_lr_containt_details c, m_goods g, m_office o, t_lr_header h " + 
 			"INNER JOIN m_clients cl1 ON cl1.client_id = h.consignor " + 
 			"INNER JOIN m_clients cl2 ON cl2.client_id = h.consignee_id " + 
-			"WHERE h.lr_header_id=c.lr_header_id AND c.goods_id=g.goods_id AND o.office_id=h.from_id AND h.is_used=true GROUP BY h.lr_header_id" ,nativeQuery=true)
+			"WHERE h.lr_header_id=c.lr_header_id AND c.goods_id=g.goods_id AND o.office_id=h.from_id AND h.is_used=true AND MONTH(h.lr_date)=MONTH(CURRENT_DATE()) AND YEAR(h.lr_date)=YEAR(CURRENT_DATE()) GROUP BY h.lr_header_id order by h.lr_header_id desc" ,nativeQuery=true)
 	List<GetAllLrDetails> findAllLr();
 
 	@Query(value="SELECT h.lr_header_id,h.lr_no,h.lr_date,g.goods_name AS particular,cl2.client_address,h.payment_by, SUM(c.no_of_containts) AS quantity, " + 
@@ -25,4 +26,16 @@ public interface GetAllLrDetailsRepository extends JpaRepository<GetAllLrDetails
 			"INNER JOIN m_clients cl2 ON cl2.client_id = h.consignee_id " + 
 			"WHERE h.lr_header_id=c.lr_header_id AND c.goods_id=g.goods_id AND o.office_id=h.from_id AND h.is_used=TRUE AND h.delivery_status=0 GROUP BY h.lr_header_id" ,nativeQuery=true)
 	List<GetAllLrDetails> findLrForMakeMemo();
+	
+	
+	@Query(value="SELECT h.lr_header_id,h.lr_no,h.lr_date,g.goods_name AS particular,cl2.client_address,h.payment_by, SUM(c.no_of_containts) AS quantity, " + 
+			"	h.total AS amount,c.goods_id, o.office_name, cl1.client_name AS consignor, cl2.client_name AS consignee " + 
+			"	FROM t_lr_containt_details c, m_goods g, m_office o, t_lr_header h INNER JOIN m_clients cl1 ON cl1.client_id = h.consignor " + 
+			"	INNER JOIN m_clients cl2 ON cl2.client_id = h.consignee_id " + 
+			"	WHERE h.lr_header_id=c.lr_header_id AND c.goods_id=g.goods_id AND o.office_id=h.from_id AND " + 
+			"	h.lr_header_id IN (SELECT lr_header_id FROM t_memo_details WHERE t_memo_details.memo_header_id=:memoHeaderId) AND h.is_used=TRUE GROUP BY h.lr_header_id" ,nativeQuery=true)
+	List<GetAllLrDetails> getMemoLrDetailsByMemoHeaderId(@Param("memoHeaderId")int memoHeaderId);
+	
+	
+	
 }
