4ea13c6a831f091d7302376073734593cc1c5a16
Jarves <Jarves@DESKTOP-TLMVIJ2>
2018-12-20 11:44:47 +0530

    //kaushik goods rate

---
+++ src/main/java/com/bionische/swastiktruckage/mastermodel/GoodsRate.java
@@ -0,0 +1,72 @@
+package com.bionische.swastiktruckage.mastermodel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="m_goods_rate")
+@EntityListeners(AuditingEntityListener.class)
+public class GoodsRate {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="goods_rate_id",length=11)
+	private int goodsRateId;
+	
+	@Column(name="office_id",length=11)
+	private int officeId;
+	
+	@Column(name="goods_id",length=11)
+	private int goodsId;
+	
+	@Column(name="goods_rate",length=11)
+	private float goodsRate;
+
+	public int getGoodsRateId() {
+		return goodsRateId;
+	}
+
+	public void setGoodsRateId(int goodsRateId) {
+		this.goodsRateId = goodsRateId;
+	}
+
+	public int getOfficeId() {
+		return officeId;
+	}
+
+	public void setOfficeId(int officeId) {
+		this.officeId = officeId;
+	}
+
+	public int getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public float getGoodsRate() {
+		return goodsRate;
+	}
+
+	public void setGoodsRate(float goodsRate) {
+		this.goodsRate = goodsRate;
+	}
+
+	@Override
+	public String toString() {
+		return "GoodsRate [goodsRateId=" + goodsRateId + ", officeId=" + officeId + ", goodsId=" + goodsId
+				+ ", goodsRate=" + goodsRate + "]";
+	}
+	
+	
+
+}
